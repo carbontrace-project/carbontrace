b18da533018e01e4343e45e557a061e31ab384d2
praveen kumar <praveenkoda7@gmail.com>
2026-07-21 20:11:08 +0530

    Extend CarbonTraceApplication with SpringBootServletInitializer

--- backend/src/main/java/com/carbontrace/CarbonTraceApplication.java
+++ backend/src/main/java/com/carbontrace/CarbonTraceApplication.java
@@ -2,9 +2,16 @@ package com.carbontrace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarbonTraceApplication {
+public class CarbonTraceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CarbonTraceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CarbonTraceApplication.class, args);
